896982fa0785a93d50b3524efbfe47186068a7cf
oouuyab <01073785645@naver.com>
2020-09-14 23:51:01 +0900

    :zap: MongoDB 연결 완료, getMediInfo 작성

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,15 +1,10 @@
 import { IResolvers, IResolverObject } from 'graphql-tools';
-import 'dotenv/config';
 import medicine from './models/Medicine';
 
 const resolverMap: IResolvers = {
   Query: {
-    // hi(_: void, args: void): string {
-    //   return `Hello GraphQL~!`;
-    // },
-    // getMediInfo: () => getMediInfo('병용금기', '', '', '1', '3'),
-    async getMediInfo() {
-      return await medicine.find();
+    async getMediInfo(root, { ENTP_NAME }) {
+      return await medicine.find({ ENTP_NAME: ENTP_NAME });
     }
   }
 };
